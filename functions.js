65c5b525e6b8af62604967f0a502c1e5d4d16acd
unknown <you@example.com>
2019-06-28 19:11:12 +0200

    boton1 commit

---
+++ functions.js
@@ -0,0 +1,18 @@
+//llamada de los elementos html
+/*
+eventos onClick
+recogida datos campos input
+lectura y pintado de datos input
+añadido de más productos
+suma productos :¿array?
+pintado final: suma y productos en div
+reset y todo en blanco
+
+*/
+let button1 = document.getElementById("boton1");
+let button2 = document.getElementById("boton2");
+
+//recogida inputs
+
+let recogeDatos1 = document.getElementById("name");
+let recogeDatos2 = document.getElementById("money");
